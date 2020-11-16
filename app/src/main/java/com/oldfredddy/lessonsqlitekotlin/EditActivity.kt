a3ecd3ba511ba95110fc7673117bae83db957271
Freddy <oldfredddy@gmail.com>
2020-11-16 11:55:22 +0300

    Видимость элементов

--- app/src/main/java/com/oldfredddy/lessonsqlitekotlin/EditActivity.kt
+++ app/src/main/java/com/oldfredddy/lessonsqlitekotlin/EditActivity.kt
@@ -2,10 +2,23 @@ package com.oldfredddy.lessonsqlitekotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.edit_activity.*
 
 class EditActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.edit_activity)
+    }
+
+    fun onClickAddImage(view: View) {
+
+        mainImageLayout.visibility = View.VISIBLE
+        fb_addImage.visibility = View.GONE
+    }
+
+    fun onClickDeleteImage(view: View) {
+        mainImageLayout.visibility = View.GONE
+        fb_addImage.visibility = View.VISIBLE
     }
 }
